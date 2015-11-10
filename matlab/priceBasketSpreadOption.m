4bd6b567067479296ec159b56c68434074bc7c93
Daniel Waelchli <whitestguyiknow@github.com>
2015-11-10 17:09:02 +0100

    started with second order bound approx

--- matlab/priceBasketSpreadOption.m
+++ matlab/priceBasketSpreadOption.m
@@ -13,13 +13,14 @@ function [V] = priceBasketSpreadOption(K, r, T, e, a, S0, sigma, rho, method, va
 % sigma:        volatility
 % rho:          correlation
 % method:       method applied for pricing
-% varagin{1}:   number of Monte Carlo paths per asset (only MC simulation)
-% varargin{2}:  number of steps per path (only MC simulation)
+% varagin{1}:   number of Monte Carlo paths per asset (only for method 'MC')
+% varargin{2}:  number of steps per path (only for method 'MC')
 
 
-%% Assertion 
+%% Assertion
 M = length(e);
 assert(M == length(a), 'number of weights incorrect');
+assert(all(abs(e)==1), 'all signs in the spread must be +1 or -1');
 assert(M == length(S0), 'number of initial values incorrect');
 assert(M == length(sigma), 'number of variances incorrect');
 assert(issymmetric(rho), 'correlation matrix not symmetric');
@@ -27,18 +28,32 @@ assert(all(eig(rho)>=zeros(M,1)), 'correlation matrix not positive-semidefinite'
 
 
 %% Computation
-switch method 
+switch method
+    case 'SB'
+        disp('Price basked-spread option with second order boundary approximation');
+        
+        I = (e==1);
+        N = sum(I);
+        S0 = S0.*a;
+        % Approximate H0(T)
+        vH0 = sqrt(sigma(I)*rho(I)*sigma(I)')/N;
+        uH0 = log(sum(exp(log(S0(I))+r*T+0.5*S0(I).^2)))-0.5*vH0^2;
+        sigma_10 = sum(rho(:,I).*sigma(I),1)/(N*vH0);
+        sigma_11 = rho(I,I);
+        rho = zeros(M+1);
+
+        
+        V=0;
+        
+        
     case 'HybMMICUB'
         disp('Price basked-spread option with hybrid moment matching method with ICUB');
         V=0;
         
-    case 'SB'
-        disp('Price basked-spread option with second order boundary approximation');
-        V=0;
-        
-    otherwise
+    case 'MC'
         disp('Price basked-spread option with Monte Carlo simulation..');
         assert(nargin==11, 'incorrect number of arguments');
+        
         nSamples = varargin{1};
         nSteps = varargin{2};
         
@@ -65,13 +80,17 @@ switch method
                     0.5*S(:,j).*sigma'.^2.*(B(:,j).^2-dt);
             end
             
-            v(i) = sum((S(:,end)'.*e.*a))-K;
+            v(i) = max(sum((S(:,end)'.*e.*a))-K,0);
             
-
             
         end
         
-        fprintf('%d standard error\n',std(v))
-        V = exp(-r*T)*mean(v);
+    otherwise
+        disp('This method is not available..');
+        
+end
+
+fprintf('%d standard error\n',std(v))
+V = exp(-r*T)*mean(v);
 end
 
