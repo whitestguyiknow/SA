0ec4493d0d929e43cb4d940e48e4bcceb5ceb8d4
Daniel Waelchli <whitestguyiknow@github.com>
2015-11-17 17:00:20 +0100

    finished HybMMICUB method

--- matlab/Main_BasketSpreadOptionPricing.m
+++ matlab/Main_BasketSpreadOptionPricing.m
@@ -2,8 +2,9 @@
 % Author: Daniel Wälchli
 % November 2015
 
+clear all;
 
-seed = 1337;
+seed = 133781;
 rng(seed);
 
 r = 0.05;
@@ -20,18 +21,20 @@ rho = [1 0.2 0.8; 0.2 1 0.4; 0.8 0.4 1];
 
 V_sb = priceBasketSpreadOption_SecondOrderBoundaryApprox(K,r,T,e,a,S0,sigma,rho)
 V_HybMMICUB = priceBasketSpreadOption_HybMMICUB(K,r,T,e,a,S0,sigma,rho)
-V_mc = priceBasketSpreadOption_MonteCarlo(K,r,T,e,a,S0,sigma,rho,1e4,3e3);
+V_mc = priceBasketSpreadOption_MonteCarlo(K,r,T,e,a,S0,sigma,rho,1e4,3e3)
 
 
-% %% Numerical Experiments from Pricing and Hedging Asian Basket Spread Options
-% K = 15;
-% T = 1;
-% 
-% e = [1 -1 -1];
-% a = [1 1 1];
-% S0 = [100 24 46];
-% sigma = [0.4 0.22 0.3];
-% rho = [1 0.17 0.91; 0.17 1 0.41; 0.91 0.41 1];
-% V_mc = priceBasketSpreadOption_MonteCarlo(K,r,T,e,a,S0,sigma,rho,'MC',1e6,3e3);
+%% Numerical Experiments from Pricing and Hedging Asian Basket Spread Options
+K = 15;
+T = 1;
+
+e = [1 -1 -1];
+a = [1 1 1];
+S0 = [100 24 46];
+sigma = [0.4 0.22 0.3];
+rho = [1 0.17 0.91; 0.17 1 0.41; 0.91 0.41 1];
+V_sb = priceBasketSpreadOption_SecondOrderBoundaryApprox(K,r,T,e,a,S0,sigma,rho)
+V_HybMMICUB = priceBasketSpreadOption_HybMMICUB(K,r,T,e,a,S0,sigma,rho)
+V_mc = priceBasketSpreadOption_MonteCarlo(K,r,T,e,a,S0,sigma,rho,1e4,3e3)
 
 
--- matlab/priceBasketSpreadOption_HybMMICUB.m
+++ matlab/priceBasketSpreadOption_HybMMICUB.m
@@ -14,6 +14,7 @@ function [V] = priceBasketSpreadOption_HybMMICUB(K, r, T, e, a, S0, sigma, rho)
 % S0:           initial value of asset
 % sigma:        volatility
 % rho:          correlation
+% nIntervall:   number of intervalls used in numerical integration
 
 %% Assertion
 N = length(e);
@@ -27,7 +28,7 @@ assert(all(eig(rho)>=zeros(N,1)), 'correlation matrix not positive-semidefinite'
 
 
 %% Computation
-disp('Price basked-spread option with second order boundary approximation');
+disp('Price basked-spread option with Hybrid Moment Matching Method associated with ICUB');
 
 I = (e==1);
 M = sum(I);
@@ -52,24 +53,71 @@ crm = sum(sum(F0(I)'*F0(~I).*exp(0.5*T*(2*rho(I,~I).*(sigma(I)'*sigma(~I)))))); 
 p = (log(crm)-u1-u2-0.5*var1-0.5*var2)/sqrt(var1*var2);
 
 % Proposition 5
-gamma1 = (exp(u1)*sqrt(var1)+exp(u2)*sqrt(var2)*p)/(exp(2*u1)*var1+exp(2*u2)*var2+2*exp(u1+u2)*sqrt(var1)*sqrt(var2));
-gamma2 = exp(u1)*sqrt(var1)*p+exp(u2)*sqrt(var2);
+gamma1 = (exp(u1)*sqrt(var1)+exp(u2)*sqrt(var2)*p)/sqrt(exp(2*u1)*var1+exp(2*u2)*var2+2*p*exp(u1+u2)*sqrt(var1)*sqrt(var2));
+gamma2 = (exp(u1)*sqrt(var1)*p+exp(u2)*sqrt(var2))/sqrt(exp(2*u1)*var1+exp(2*u2)*var2+2*p*exp(u1+u2)*sqrt(var1)*sqrt(var2));
 Y1 = sqrt(var1)*sqrt(1-gamma1^2);
-Y2 = sqrt(var2)*sqrt(1-gamma2^2);
+Y2 = -sqrt(var2)*sqrt(1-gamma2^2);
 A1 = @(u) gamma1*sqrt(var1)*norminv(u,0,1);
 A2 = @(u) gamma2*sqrt(var2)*norminv(u,0,1);
-fun = @(x,u) exp(u1+A1(u)+Y1*x)+exp(u2+A2(u)+Y2*x)-K;
-x=linspace(0,1,100)
-subplot(2,2,1)
-plot(x,fun(x,0.2))
-subplot(2,2,2)
-plot(x,fun(x,0.5))
-subplot(2,2,3)
-plot(x,fun(x,0.7))
-subplot(2,2,4)
-plot(x,fun(x,0.9))
-V=0
+
+fsic = @(x) fsicu(x,u1,A1,Y1,u2,A2,Y2,K,0.5);
+FSICK = adaptive_simpson_rule(fsic,0.001,0.999,1e-8,simpsons_rule(fsic,0.00001,0.99999),1);
+
+ifsic_1 = @(x) integrandFsicu_1(x,u1,A1,Y1,u2,A2,Y2,K,0.5);
+ifsic_2 = @(x) integrandFsicu_2(x,u1,A1,Y1,u2,A2,Y2,K,0.5);
+dx=1e-7;
+I1 = adaptive_simpson_rule(ifsic_1,dx,1-dx,1e-8,simpsons_rule(ifsic_1,dx,1-dx),1);
+I2 = adaptive_simpson_rule(ifsic_2,dx,1-dx,1e-8,simpsons_rule(ifsic_2,dx,1-dx),1);
+
+V=I1-I2-K*(1-FSICK);
 end
 
+function [F] = fsicu(u,u1,A1,Y1,u2,A2,Y2,K,x0)
+    f = @(x) exp(u1+A1(u)+Y1*x)-exp(u2+A2(u)+Y2*x)-K;
+    F = fzero(f,x0);
+    F = normcdf(F);
+end
 
+function [I1] = integrandFsicu_1(u,u1,A1,Y1,u2,A2,Y2,K,x0)
+    f = @(x) exp(u1+A1(u)+Y1*x)-exp(u2+A2(u)+Y2*x)-K;
+    F = fzero(f,x0);
+    I1 = exp(u1+A1(u)+0.5*Y1^2)*normcdf(Y1-F);
+end
 
+function [I2] = integrandFsicu_2(u,u1,A1,Y1,u2,A2,Y2,K,x0)
+    f = @(x) exp(u1+A1(u)+Y1*x)-exp(u2+A2(u)+Y2*x)-K;
+    F = fzero(f,x0);
+    I2 = exp(u2+A2(u)+0.5*Y2^2)*normcdf(Y2-F);
+end
+
+function [I] = simpson(f,a,b,N)
+    % Numerical Integration with Simpson's Rule
+    if(mod(N,2))
+        N = N+1;
+    end
+    dx = (b-a)/(N-1);
+    x = linspace(a,b,N);
+    F = zeros(1,N);
+    F(2:end-1) = arrayfun(f,x(2:end-1));
+    F(1) = F(2);
+    F(end) = F(end-1);
+    %plot(x,F)
+    I = dx/3*(4*sum(F(2:2:end-1))+2*sum(F(3:2:end-3))+F(1)+F(end));
+end
+
+function [I] = simpsons_rule(f,a,b)
+    c = (a+b)/2;
+    h = (b-a)/6;
+    I = h*(f(a)+4.0*f(c)+f(b));
+end
+
+function [I] = adaptive_simpson_rule(f,a,b,eps,base,N)
+    c = (a+b)/2;
+    l = simpsons_rule(f,a,c);
+    r = simpsons_rule(f,c,b);
+    if (abs(l + r - base) <= 15*eps)
+        I = l + r + (l + r - base)/15.0;
+    else
+        I = adaptive_simpson_rule(f,a,c,eps/2.0,l,N+1) + adaptive_simpson_rule(f,c,b,eps/2.0,r,N+1);
+    end
+end
