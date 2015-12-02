ad6226c4aae928a2f1e5d4027ce51684d38de6d8
Daniel Waelchli <wadaniel@student.ethz.ch>
2015-12-02 02:04:14 +0100

    added plots

--- matlab/Plotting.m
+++ matlab/Plotting.m
@@ -9,19 +9,19 @@ r = 0.05;
 T = 1;
 
 % grids
-M = 500;
+M = 100;
 x = linspace(-5,5,M);
 dx = 1e-12;
 u = linspace(dx,1-dx,M);
 
 % cases for u
-u0=0.3;
-u1=0.7;
+u0=0.2;
+u1=0.8;
 
 % market params
 [e_0,a_0,S0_0,sigma_0,rho_0] = generateMarketParams(4,2,'charged','constant',0.4,'constant',0.3);
 [e_1,a_1,S0_1,sigma_1,rho_1] = generateMarketParams(20,5,'descending','constant',0.4,'alternating',0.3);
-[e_2,a_2,S0_2,sigma_2,rho_2] = generateMarketParams(10,2,'charged','constant',0.4,'descending',0.3);
+[e_2,a_2,S0_2,sigma_2,rho_2] = generateMarketParams(10,6,'charged','constant',0.4,'descending',0.3);
 
 %% evaluation of f(x)
 f0_0 = zeros(1,M);
@@ -47,16 +47,18 @@ end
 
 figure(1)
 hold on;
+grid minor;
 plot(x,f0_0,'b',x,f1_0,'--b',x,f0_1,'r',x,f1_1,'--r',x,f0_2,'g',x,f1_2,'--g')
-grid on;
 xlabel('x','FontName','Cambria','FontSize',14);
 ylab=ylabel('f(x)','FontName','Cambria','FontSize',14,'rot',0);
-set(ylab,'horizontalAlignment', 'left');
-title('EVALUATION F(X,U=0.3) AND F(X,U=0.7)','FontName','Cambria','FontSize',16);
-legend({'scenario 1, u0','scenario 1, u1','scenario 2, u0',...
-    'scenario 2, u1','scenario 3, u10','scenario 3, u1'},'FontSize',12,...
-    'Location','southeast')
+set(ylab,'horizontalAlignment', 'left','position', [-5.75, 1040, 0]);
+%title('EVALUATION F(X,U=0.2) AND F(X,U=0.8)','FontName','Cambria','FontSize',16);
+legend({'scenario 1, u=0.2','scenario 1, u=0.8','scenario 2, u=0.2',...
+    'scenario 2, u=0.8','scenario 3, u=0.2','scenario 3, u=0.8'},'FontSize',12,...
+    'Location','southeast');
+axis('square');
 hold off;
+export_fig('1','-transparent','-pdf');
 
 %% evaluation of integrands
 I1_0 = zeros(1,M);
@@ -83,12 +85,15 @@ end
 figure(2)
 hold on;
 plot(u,I1_0,'b',u,I2_0,'--b',u,I1_1,'r',u,I2_1,'--r',u,I1_2,'g',u,I2_1,'--g')
-grid on;
+grid minor;
 xlabel('x','FontName','Cambria','FontSize',14);
+ylim([0,1000])
 ylab=ylabel('I_i(x)','FontName','Cambria','FontSize',14,'rot',0);
-set(ylab,'horizontalAlignment', 'left');
-title('Integrands','FontName','Cambria','FontSize',16);
-legend({'scenario 1, I1','scenario 1, I2','scenario 2, I1',...
-    'scenario 2, I2','scenario 3, I1','scenario 3, I2'},'FontSize',12,...
-    'Location','northwest')
+set(ylab,'horizontalAlignment', 'left','position', [-0.075, 1010, 0]);
+%title('Integrands','FontName','Cambria','FontSize',16);
+legend({'scenario 1, I_1','scenario 1, I_2','scenario 2, I_1',...
+    'scenario 2, I_2','scenario 3, I_1','scenario 3, I_2'},'FontSize',12,...
+    'Location','northwest');
+axis('square');
 hold off;
+export_fig('2','-transparent','-pdf');
